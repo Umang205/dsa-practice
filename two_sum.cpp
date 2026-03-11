03eb6ce749c8c4176ceaf7b414bc0473f0ceb0da
Umang <152995225+Umang205@users.noreply.github.com>
2026-03-11 18:43:17 +0530

    Update two_sum.cpp

--- two_sum.cpp
+++ two_sum.cpp
@@ -1,1 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> nums = {1, 2, 4, 7, 11, 15};
+    int target = 15;
+
+    int left = 0;
+    int right = nums.size() - 1;
+
+    while(left < right) {
+        int sum = nums[left] + nums[right];
+
+        if(sum == target) {
+            cout << nums[left] << " " << nums[right];
+            return 0;
+        }
+        else if(sum < target) {
+            left++;
+        }
+        else {
+            right--;
+        }
+    }
+
+    cout << "No pair found";
+    return 0;
+}
 
